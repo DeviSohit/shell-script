f6ed3139f02d17eef9bf408dad6f24c2617a25d0
devi <devimeena.iiit@gmail.com>
2023-08-16 16:03:08 +0530

    variables script added

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #promting the username and password by using read command
-echo "Please enter you username"
-read USERNAME
-echo "Please enter you password"
-read PASSWORD
+echo -n "Please enter you username"
+read -s USERNAME
+echo -n "Please enter you password"
+read -s PASSWORD
+echo "username is : $USERNAME"
+echo "password is : $PASSWORD"
