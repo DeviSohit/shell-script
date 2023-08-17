c5d258f6ea793903668b3b3e9fc2df659340c548
devi <devimeena.iiit@gmail.com>
2023-08-17 19:22:30 +0530

    committing script

--- 09-functions.sh
+++ 09-functions.sh
@@ -10,25 +10,25 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
-VALIDATE(){
-
+VALIDATE(){}
 if [ $1 -ne 0 ] ; then
     echo -e "$2.......$R FAILURE $N"
     exit 1
 else 
     echo -e "$2.......$G SUCCESS $N"
 fi
-
 }
-
 USERID=$(id -u)
 if [ $USERID -ne 0 ] ; then
     echo "Please take root access to run this script"
     exit 1
 fi
-
+if [ $INSTALLED_FILE --installed ] ; then
+    echo -e "$INSTALLED_FILE.......already installed"
+    exit 1
+fi
 yum install mysql -y &>> $LOGFILE
 VALIDATE $? "Installing mysql"
 
-yum install posttfix -y &>> $LOGFILE
+yum install postfix -y &>> $LOGFILE
 VALIDATE $? "Installing postfix"
