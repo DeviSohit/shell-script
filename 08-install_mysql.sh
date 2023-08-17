cc38761e8186da55885ca4fe7f62e6faffade50e
devi <devimeena.iiit@gmail.com>
2023-08-17 12:33:39 +0530

    committing script

---
+++ 08-install_mysql.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+USERID=$(id -u)
+if [ $USERID -ne 0 ] ; then
+    echo "Please take root access to run this script"
+    exit 1
+fi
+
+yum install mysql -y
+
+if [ $? -ne 0 ] ; then
+    echo "Installation is failure"
+    exit 1
+else 
+    echo "Installation is success"
+fi
