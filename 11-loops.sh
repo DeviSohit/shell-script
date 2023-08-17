c081475b3ff9878b04ed15dd046f0f5d9b76de49
devi <devimeena.iiit@gmail.com>
2023-08-17 13:39:34 +0530

    committing script

--- 11-loops.sh
+++ 11-loops.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
-#for i in {1..100}
-#do
-  #  echo $i
-#done
-VALIDATE(){
-
-if [ $1 -ne 0 ] ; then
-    echo -e "$@.......$R FAILURE $N"
-    exit 1
-else 
-    echo -e "$@.......$G SUCCESS $N"
-fi
-}
-for i in $@
+for i in {1..100}
 do
-  yum install $i -y
+   echo $i
 done
-VALIDATE $? "Installing $@"
