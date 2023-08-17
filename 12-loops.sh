fe70ed338182b5efdb22402647afe7fe46e1a84c
devi <devimeena.iiit@gmail.com>
2023-08-17 20:11:31 +0530

    committing script

--- 12-loops.sh
+++ 12-loops.sh
@@ -27,9 +27,6 @@ if [ $USERID -ne 0 ] ; then
 fi
 for i in $@
 do
-    if [ $@ -i ] ; then
-    echo "$@ is already installed"
-    fi
     yum install $i -y &>> $LOGFILE
     VALIDATE $? "Installing $@"
 done
