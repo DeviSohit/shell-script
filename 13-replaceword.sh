8f151f6c1034a7aff1b199010ec49b06851ecaf9
devi <devimeena.iiit@gmail.com>
2023-08-18 09:55:49 +0530

    commit script again

--- 13-replaceword.sh
+++ 13-replaceword.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DIRECTORY=/home/centos/devi.txt
-REPLACE=$(echo $DIRECTORY/sed -e 's/sbin/sohit')
+REPLACE=$(echo $DIRECTORY | sed -e 's/sbin/sohit')
 echo "the output file is $REPLACE"
