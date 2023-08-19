f6527c69c573d70d2fe5d1abda0b26f25ce989a7
devi <devimeena.iiit@gmail.com>
2023-08-19 20:47:03 +0530

    adding script to delete old logs

--- 13-delete-old-logs.sh
+++ 13-delete-old-logs.sh
@@ -3,6 +3,12 @@ APP_LOG_DIR=/home/centos/app-logs
 
 DATE=$(date +%F)
 LOGFILE=/home/centos/shellscript-logs/$SCRIPT_NAME-$DATE.log
-FILE_TO_DELETE=$(find $APP_LOG_DIR -name "*.log" -type f -mtime +14)
+FILES_TO_DELETE=$(find $APP_LOG_DIR -name "*.log" -type f -mtime +14)
 
-echo "$FILE_TO_DELETE"
+echo "2 weeks older files are $FILES_TO_DELETE"
+
+while read line
+do 
+    echo "Deleting $line"
+    rm -rf &>> $LOGFILE
+done <<< $FILES_TO_DELETE
