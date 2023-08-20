cbebbe22298456e7dd3cc6460915616c03a6b408
devi <devimeena.iiit@gmail.com>
2023-08-20 10:09:19 +0530

    commit replacing text

--- 14-replace-text.sh
+++ 14-replace-text.sh
@@ -3,5 +3,5 @@ DIRECTORY=/home/centos/replace.txt
 
 Y="\e[33m"
 N="\e[0m"
-REPLACE_WORD=$(sed -i 's/sbin/devi/g' $DIRECTORY)
-echo "replacing text $REPLACE_WORD"
+REPLACE_WORD=$(sed -i 's/sbin/$Y devi $N/g' $DIRECTORY)
+echo "replacing text"
