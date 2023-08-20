35a7dc286a0d9a3f39bf23e010a1fd3c0b58fed6
devi <devimeena.iiit@gmail.com>
2023-08-20 10:05:54 +0530

    commit replacing text

--- 14-replace-text.sh
+++ 14-replace-text.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 DIRECTORY=/home/centos/replace.txt
-#REPLACE_WORD=$(sed -e 's/sbin/devi/g')
+
 Y="\e[33m"
 N="\e[0m"
-while read line
-do 
-    echo "replacing text globally"
-   # echo -e "$Y $REPLACE_WORD $line $N"
-   $(sed -i 's/sbin/devi') $line
-done <<< $DIRECTORY
+REPLACE_WORD=$(sed -i 's/sbin/devi/g' $DIRECTORY)
+echo "replacing text $REPLACE_WORD"
