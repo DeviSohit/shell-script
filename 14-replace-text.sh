9baef662124754b084f2d121c728d91d0966b8cd
devi <devimeena.iiit@gmail.com>
2023-08-20 10:50:08 +0530

    commit replacing text

--- 14-replace-text.sh
+++ 14-replace-text.sh
@@ -4,6 +4,6 @@ WORD1=$1
 WORD2=$2
 Y="\e[33m"
 N="\e[0m"
-echo -e "$Y $WORD2 $N"
-REPLACE_WORD=$(sed -i s/$WORD1/$WORD2/g $DIRECTORY)
+#echo -e "$Y $WORD2 $N"
+REPLACE_WORD=$(sed -i s/$WORD1/echo -e "$Y $WORD2 $N"/g $DIRECTORY)
 echo "replacing text $(cat $DIRECTORY)"
