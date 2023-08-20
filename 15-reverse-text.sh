98ec45ad308f8c9c8d5409b52bbacfeeeb8ac1bd
devi <devimeena.iiit@gmail.com>
2023-08-20 11:43:20 +0530

    commit reverse text

--- 15-reverse-text.sh
+++ 15-reverse-text.sh
@@ -3,7 +3,7 @@
 #read STRING
 #REV=$(echo $STRING | rev)
 #echo "$REV"
-FILE_DIRECTORY=/home/centos/reverse.txt
+FILE_DIRECTORY=/tmp/reverse.txt
 while read line
 do
     echo "reversing text $line"
